be0ea73d9c2e3cc1af26e71c401ade219c5469e2
bhavana25 <36507131+bhavana25@users.noreply.github.com>
2018-02-21 19:27:15 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -8,14 +8,14 @@ app.use(morgan('combined'));
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-app.get('/artical-one', function (req, res) {
-  res.sendFile('artical one is requested and will be served here');
+app.get('/article-one', function (req, res) {
+  res.sendFile('article one is requested and will be served here');
 });
-app.get('/artical-two', function (req, res) {
-  res.sendFile('artical two is requested and will be served here');
+app.get('/article-two', function (req, res) {
+  res.sendFile('article two is requested and will be served here');
 });
-app.get('/artical-three', function (req, res) {
-  res.sendFile('artical three is requested and will be served here');
+app.get('/article-three', function (req, res) {
+  res.sendFile('article three is requested and will be served here');
 });
 
 app.get('/ui/style.css', function (req, res) {
