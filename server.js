70da59c54d97eb4c1e8df676755d9a219cfbc925
bhavana25 <36507131+bhavana25@users.noreply.github.com>
2018-02-21 19:25:29 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/artical-one', function (req, res) {
-  res.sendFile(path.join('artical one is requested and will be served here'));
+  res.sendFile('artical one is requested and will be served here');
 });
 app.get('/artical-two', function (req, res) {
-  res.sendFile(path.join('artical two is requested and will be served here'));
+  res.sendFile('artical two is requested and will be served here');
 });
 app.get('/artical-three', function (req, res) {
-  res.sendFile(path.join('artical three is requested and will be served here'));
+  res.sendFile('artical three is requested and will be served here');
 });
 
 app.get('/ui/style.css', function (req, res) {
